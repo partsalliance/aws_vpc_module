e72c5600af09e91f11537191f87bd981060c1056
partsalliance <patrickcroot@gmail.com>
2017-05-20 14:31:24 +0100

    remove variables as turning into a module

--- variables.tf
+++ variables.tf
@@ -1,30 +1,12 @@
-variable "aws_access_key" {}
-variable "aws_secret_key" {}
+variable "name" {}
 
-variable "name" {
-	description = "name of everything in here"
-	default = "alpha"
-}
+variable "region" {}
 
-variable "region" {
-	description = "AWS region"
-	default = "us-west-2"
-}
+variable "vpc_cidr" {}
 
-variable "vpc_cidr" {
-	description = "VPC cidr for tests"
-	default = "172.20.0.0/24"
-}
+variable "public_subnet_cidr" {}
 
-variable "public_subnet_cidr" {
-	description = "CIDR for public subnet"
-	default = "172.20.0.0/26"
-}
-
-variable "private_subnet_cidr" {
-	description = "CIDR for private subnet"
-	default = "172.20.0.64/26"
-}
+variable "private_subnet_cidr" {}
 
 variable "tags" {
 	type = "map"
