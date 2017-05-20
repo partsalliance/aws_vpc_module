a52af459b25496b30a22cf3918d575ed76229d15
partsalliance <patrickcroot@gmail.com>
2017-05-20 14:51:37 +0100

    add output of subnet ids

---
+++ outputs.tf
@@ -0,0 +1,11 @@
+output "vpc_id" {
+	value = "${aws_vpc.vpc_id}"
+}
+
+output "public_subnet_cidr" {
+	value = "${aws_subnet.public-subnet.id}"
+}
+
+output "private_subnet_cidr" {
+	value = "${aws_subnet.private-subnet.id}"
+}
